9a6f66651eec4cb32bf140deedb9aadef73c07e8
koyama86 <h.koyama.sys23@morijyobi.ac.jp>
2024-03-25 02:40:02 +0900

    課題のコミット

---
+++ practice_6/fontChanges/change.js
@@ -0,0 +1,42 @@
+
+
+
+function btn(){
+    let a  =document.getElementById("font").value;
+    let res=document.getElementById("re");
+    res.textContent=a;
+    res.style.cssText="color:black;font-size:1rem;";
+
+}
+function btn1(){
+    let a  =document.getElementById("font").value;
+    let res=document.getElementById("re");
+    res.textContent=a;
+    res.style.cssText="color:black;font-size:1px;";
+}
+function btn2(){
+    let a  =document.getElementById("font").value;
+    let res=document.getElementById("re");
+    res.textContent=a;
+    res.style.cssText="color:red;font-size:10px;";
+
+}
+function btn3(){
+    let a  =document.getElementById("font").value;
+    let res=document.getElementById("re");
+    res.textContent=a;
+    res.style.cssText="color:blue;font-size:100px;";
+
+}
+function btn4(){
+    let a  =document.getElementById("font").value;
+    let res=document.getElementById("re");
+    res.textContent=a;
+    res.style.cssText=` background: linear-gradient(to right,#e60000,#f39800,#fff100,#009944,#0068b7,#1d2088,#920783);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: 300%;
+    font-weight: bold;
+    display: inline-block;font-size:400px;`;
+
+}
